39c74ecbfb9e0a1019ad4832a4905c2fdfbe0b3c
ZijiaJiang <98132324+ZijiaJiang@users.noreply.github.com>
2023-02-27 01:59:06 +0000

    successful add new user, need to deal with data

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
-use actix_web::{get, post, HttpResponse, Responder};
+use std::{fs::File, io::Write, path};
+
+use actix_web::{get, post, web, HttpResponse, Responder};
+
+#[derive(serde::Deserialize)]
+pub struct User {
+    pub name: String,
+    pub email: String,
+    pub tel: String,
+}
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -12,4 +21,27 @@ async fn add_new() -> impl Responder {
     HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
         .body(include_str!("../templates/add_new.html"))
+}
+
+#[get("/addNew/{name}/{email}/{tel}")]
+async fn submit(path: web::Path<(String, String, String)>) -> impl Responder {
+    // Extract the tuple from the web::Path struct using into_inner()
+    let (name, email, tel) = path.into_inner();
+
+    // create a new user
+    let user = User {
+        name,
+        email,
+        tel,
+    };
+
+    // open the file ./data.txt to write the user data
+    let mut file = File::create("./data.txt").expect("Failed to create file");
+    // write the user data to a file
+    file.write_all(format!("{} {} {}", user.name, user.email, user.tel).as_bytes())
+        .unwrap();
+
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("../templates/submit.html"))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 // web microservice that manager users profiles
 use actix_web::{App, HttpServer};
-use prj2::{add_new, index};
+use prj2::{add_new, index, submit};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index).service(add_new))
+    HttpServer::new(|| App::new().service(index).service(add_new).service(submit))
         .bind("127.0.0.1:8080")?
         .run()
         .await
